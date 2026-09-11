981625ce064c59a654b4868f564795db16941ad6
SyntaxBawa <nikoz.faeldin23@gmail.com>
2026-09-11 13:53:28 +0800

    https://github.com/SyntaxBawa/PHP.git

---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP PAGE</title>
+</head>
+<body>
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name">
+    <br><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email">
+    <br><br>
+    Hobbies:
+    <br><br>
+    <labe
+
+    
+
+</body>
+</html>
